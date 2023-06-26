d7e9d16f4800bc6df186ddbe879662ee57ba24d0
Ricardo-ChoqueMendez1994 <choquemendezr@gmail.com>
2023-06-26 17:37:49 -0500

    first commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs'); // Establecer express para utilizar plantillas ejs
 app.use('/public/', express.static('./public')); // Cargar archivos estáticos desde la carpeta 'public'
 
-const port = 3000;
+const port = 80;
 
 const dbConfig = {
   connectionLimit: 100,
